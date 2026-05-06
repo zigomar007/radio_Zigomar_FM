4b30af4431c1952d27fbbef674f31867b25b28d6
Dali <103475660+zigomar007@users.noreply.github.com>
2026-05-06 20:56:14 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import 'https://zigomar007.github.io/radio_Zigomar_FM/src/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
